b78a893cc5ef82dd7cfc2bfc9663c08bd36bf8e7
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2021-05-05 11:51:53 +0530

    fix: date converted to utc

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,7 +7,7 @@ function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <div>{props.date}</div>
+      <div>{props.date.toUTCString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
