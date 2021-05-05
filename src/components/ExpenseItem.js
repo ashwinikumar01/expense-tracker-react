c6ae98445311e6270fd13e29774ba6b42712b589
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2021-05-05 12:00:09 +0530

    feat: date converted to readable format

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  // const expenseDate = new Date(2021, 2, 28);
-  // const expenseTitle = "Car Insurance";
-  // const expenseAmount = 294.67;
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>{props.date.toUTCString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
