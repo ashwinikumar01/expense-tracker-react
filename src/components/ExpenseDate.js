3b4c9a6e1aa8033fbcf12dbb7f88f4076533afd8
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2021-05-05 12:20:20 +0530

    feat(refactor): ExpenseDate made to reusable component

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,15 @@
+function ExpenseDate(props) {
+  const month = props.expenseDate.toLocaleString("en-US", { month: "long" });
+  const day = props.expenseDate.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.expenseDate.getFullYear();
+
+  return (
+    <div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
